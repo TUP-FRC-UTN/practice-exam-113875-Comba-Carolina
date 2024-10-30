11852afa7bbb0af5fcc728cf2a8debb9879b7e9c
carolinacomba <carolinacomba42@gmail.com>
2024-10-30 01:57:14 -0300

    cc

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -1,36 +1,53 @@
 import { Component } from '@angular/core';
-import { FormArray, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-order',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-order.component.html',
-  styleUrl: './create-order.component.css'
+  styleUrl: './create-order.component.css',
 })
 export class CreateOrderComponent {
-
+  id: string = '';
   form: FormGroup = new FormGroup({
-    nombre: new FormGroup("", [Validators.required, Validators.minLength(3)]),
-    email: new FormGroup("", [Validators.required, Validators.email]),
-    productos: new FormArray([]),
+    customerName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    products: new FormArray([]),
   });
+  total: number = 0;
+  orderCode: string = '';
+  timestamp: number = 0;
 
-  get productos() {
-    return this.form.controls["productos"] as FormArray;
+  get products() {
+    return this.form.controls['products'] as FormArray;
   }
 
   quitarProducto(index: number) {
-    this.productos.removeAt(index);
+    this.products.removeAt(index);
   }
 
+  productId: number = 0;
   agregarProducto() {
-    this.productos.push(new FormGroup({
-      nombre: new FormGroup("", Validators.required),
-      cantidad: new FormGroup("", Validators.required),
-      precio: new FormGroup("", Validators.required),
-      stock: new FormGroup("", Validators.required),
-    }));
+    this.products.push(
+      new FormGroup({
+        quantity: new FormControl('', Validators.required),
+        stock: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required),
+      })
+    );
   }
 
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
