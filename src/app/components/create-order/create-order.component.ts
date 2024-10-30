d7fd32a02ca2f75d99bd1594d5dad1b77fb7522b
carolinacomba <carolinacomba42@gmail.com>
2024-10-30 03:10:18 -0300

    .

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   FormArray,
   FormControl,
@@ -6,16 +6,27 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Producto } from '../../models/producto';
+import { ProductsService } from '../../services/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-order',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.css',
 })
-export class CreateOrderComponent {
-  id: string = '';
+export class CreateOrderComponent implements OnInit {
+  private productService = inject(ProductsService);
+  productos: Producto[] = [];
+
+  ngOnInit(): void {
+    this.productService.getProductos().subscribe((productos) => {
+      this.productos = productos;
+    });
+  }
+
   form: FormGroup = new FormGroup({
     customerName: new FormControl('', [
       Validators.required,
@@ -24,30 +35,48 @@ export class CreateOrderComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     products: new FormArray([]),
   });
-  total: number = 0;
-  orderCode: string = '';
-  timestamp: number = 0;
 
   get products() {
-    return this.form.controls['products'] as FormArray;
+    return this.form.get('products') as FormArray;
   }
 
   quitarProducto(index: number) {
     this.products.removeAt(index);
   }
 
-  productId: number = 0;
   agregarProducto() {
-    this.products.push(
-      new FormGroup({
-        quantity: new FormControl('', Validators.required),
-        stock: new FormControl('', Validators.required),
-        price: new FormControl('', Validators.required),
-      })
-    );
+    const productGroup = new FormGroup({
+      productId: new FormControl(''),
+      quantity: new FormControl('', [Validators.required, Validators.min(1)]),
+      stock: new FormControl(
+        { value: '', disabled: true },
+        Validators.required
+      ),
+      price: new FormControl(
+        { value: '', disabled: true },
+        Validators.required
+      ),
+    });
+    this.products.push(productGroup);
+  }
+
+  onProductSelect(index: number) {
+    const productGroup = this.products.at(index) as FormGroup;
+    const selectedProductId = productGroup.get('productId')?.value;
+    const selectedProduct = this.productos.find(p => p.id === selectedProductId);
+
+    if (selectedProduct) {
+      productGroup.patchValue({
+        price: selectedProduct.price,
+        stock: selectedProduct.stock,
+      });
+    }
   }
 
   onSubmit() {
-    throw new Error('Method not implemented.');
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
   }
+  
 }
