40f7fb96b0c91c82982cab11eaa7435d20c9dc60
carolinacomba <carolinacomba42@gmail.com>
2024-10-30 21:45:38 -0300

    cambio estilos

--- src/app/components/list-orders/list-orders.component.ts
+++ src/app/components/list-orders/list-orders.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Orden } from '../../models/orden';
 import { OrdersService } from '../../services/orders.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './list-orders.component.html',
   styleUrl: './list-orders.component.css'
 })
